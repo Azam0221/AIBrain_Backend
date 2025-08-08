b5654cf19be3abe54137ef2da14d8f1f39bb401d
Mohammad Azam <mohdazam0311@gmail.com>
2025-08-08 11:10:49 +0530

    Authentication update - 1.1

--- src/main/java/com/example/telusko/spring_aibrain_backend/model/User.java
+++ src/main/java/com/example/telusko/spring_aibrain_backend/model/User.java
@@ -1,53 +1,26 @@
 package com.example.telusko.spring_aibrain_backend.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
 
 @Entity
+@Data
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private int id;
-    private String username;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
     private String password;
-
-    public User() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
